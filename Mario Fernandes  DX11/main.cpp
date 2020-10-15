502daa30febc6f4c846a49bb21b06260b1851802
Mário <ask.mariofernandes@gmail.com>
2020-10-15 16:39:54 +0100

    shaders \ vertices \ buffer

--- Mario Fernandes  DX11/main.cpp
+++ Mario Fernandes  DX11/main.cpp
@@ -9,26 +9,80 @@
 #include <d3dx11.h>
 #include <windows.h>
 #include <dxerr.h>
+#define _XM_NO_INTINSICS_
+#define XM_NO_ALIGNMENT
+#include <DirectXMath.h>
+
+using namespace DirectX;
+
+#pragma region Global Variables
+
+HINSTANCE g_hInst = NULL;
+HWND g_hWnd = NULL;
+
+D3D_DRIVER_TYPE g_driverType = D3D_DRIVER_TYPE_NULL;
+D3D_FEATURE_LEVEL g_featrureLevel = D3D_FEATURE_LEVEL_11_0;
+ID3D11Device* g_pD3DDevice = NULL;
+ID3D11DeviceContext* g_pImmediateContext = NULL;
+IDXGISwapChain* g_pSwapChain = NULL;
+
+ID3D11RenderTargetView* g_pBackBufferRTView = NULL;
+
+ID3D11Buffer* g_pVertexBuffer;
+ID3D11VertexShader* g_pVertexShader;
+ID3D11PixelShader* g_pPixelShader;
+ID3D11InputLayout* g_pInputLayout;
+
+#pragma endregion
+
+//Define vertex structure
+struct POS_COL_VERTEX//This will be added to and renamed in future tutorials
+{
+	XMFLOAT3	pos;
+	XMFLOAT4	Col;
+};
+
+// Windows Title
+char g_TutorialName[100] = "Tutorial 01 Exercice 01\0";
 
 #pragma region Foward declaration
 
 HRESULT InitialiseWindow(HINSTANCE hInstance, int nCmdShow);
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
+HRESULT InitialiseD3D();
+void ShutdownD3D();
+
+void RenderFrame(void);
+
+HRESULT InitialiseGraphics(void);
+
 #pragma endregion
 
 int CALLBACK WinMain(
-	HINSTANCE	hInstance,
-	HINSTANCE	HpervInstance,	// Always null
-	LPSTR		lpCmdLine,		// Recive cm line as a string
-	int			nCmdShow)		// Defines how windows is shown
+	_In_		HINSTANCE	hInstace,
+	_In_opt_	HINSTANCE	hPrevInstance,	// Always null
+	_In_		LPSTR lpCmdLine,		// Recive cm line as a string
+	_In_		int nCmdShow)		// Defines how windows is shown
 {
-	UNREFERENCED_PARAMETER(HpervInstance);
+	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
-	if (FAILED(InitialiseWindow(hInstance, nCmdShow)))
+	if (FAILED(InitialiseWindow(hInstace, nCmdShow)))
 	{
 		DXTRACE_MSG("Failed to create Windows");
+		return 0;
+	}
+
+	if (FAILED(InitialiseD3D()))
+	{
+		DXTRACE_MSG("Failed to create Device");
+		return 0;
+	}
+
+	if (FAILED(InitialiseGraphics()))
+	{
+		DXTRACE_MSG("Failed to create graphics");
 		return 0;
 	}
 
@@ -46,59 +100,53 @@ int CALLBACK WinMain(
 		else
 		{
 			//do somthing
+			RenderFrame();
 		}
 	}
+
+	ShutdownD3D();
+
 	return (int)msg.wParam;
 }
 
+//////////////////////////////////////////////////////////////////////////////
+//   Register class and create window
+//////////////////////////////////////////////////////////////////////////////
 HRESULT InitialiseWindow(HINSTANCE hInstance, int nCmdShow)
 {
-	const auto pClassName = "Mario Fernandes";
+	//Give your app your own name
+	char Name[100] = "Mario Fernandes\0";
 
-	//register window class
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;		//Get msg
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName;
-	wc.hIconSm = nullptr;
+	//Register class
+	WNDCLASSEX wcex = { 0 };
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);// Need for non-D3D apps
+	wcex.lpszClassName = Name;
 
-	RegisterClassEx(&wc);
+	if (!RegisterClassEx(&wcex)) return E_FAIL;
 
-	//create window
-
-	//Window size
+	// Create window
+	g_hInst = hInstance;
 	RECT rc = { 0, 0, 640, 480 };
-
-	// Windows Title
-	char g_TutorialName[100] = "Tutorial 01 Exercice 01\0";
-
 	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
-	HWND hWnd = CreateWindowEx(
-		0,						//Extended styles
-		pClassName,				//Class name
-		g_TutorialName,			//Window name
-		WS_OVERLAPPEDWINDOW,	//Basic styles
-		0, 0,					//Start position of windows
-		rc.right - rc.left,		//Resolution
-		rc.bottom - rc.top,		//Resolution
-		nullptr, nullptr, hInstance, nullptr
-	);
+	g_hWnd = CreateWindow(Name, g_TutorialName, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left,
+		rc.bottom - rc.top, NULL, NULL, hInstance, NULL);
 
-	if (!hWnd)
+	if (!g_hWnd)
 		return E_FAIL;
 
-	ShowWindow(hWnd, nCmdShow);
+	ShowWindow(g_hWnd, nCmdShow);
+
+	return S_OK;
 }
 
+//////////////////////////////////////////////////////////////////////////////
 //   Call every time the aplication recives a message
+//////////////////////////////////////////////////////////////////////////////
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	PAINTSTRUCT ps;
@@ -117,12 +165,253 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 	case WM_KEYDOWN:
 		if (wParam == VK_ESCAPE)
-			DestroyWindow(hWnd);
+			DestroyWindow(g_hWnd);
 		return 0;
 
 	default:
-		return DefWindowProc(hWnd, message, wParam, lParam); //Mantain the normal windows behavior
+		return DefWindowProc(hWnd, message, wParam, lParam);
 	}
 
 	return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//Create D#D device andswap chain
+//////////////////////////////////////////////////////////////////////////////
+HRESULT InitialiseD3D()
+{
+	HRESULT hr = S_OK;
+
+	RECT rc;
+	GetClientRect(g_hWnd, &rc);
+	UINT width = rc.right - rc.left;
+	UINT heigth = rc.bottom - rc.top;
+
+	UINT createDeviceFlag = 0;
+
+#ifdef _DEBUG
+	createDeviceFlag != D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+	D3D_DRIVER_TYPE driverTypes[] =
+	{
+		D3D_DRIVER_TYPE_HARDWARE, //comment aout this line if you need to test D3D 11.0 functionality on hardware that doesn't suport it
+		D3D_DRIVER_TYPE_WARP, //comment this out also to use reference device
+		D3D_DRIVER_TYPE_REFERENCE,
+	};
+	UINT numDriverTypes = ARRAYSIZE(driverTypes);
+
+	D3D_FEATURE_LEVEL featureLevels[] =
+	{
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_1,
+		D3D_FEATURE_LEVEL_10_0,
+	};
+	UINT numFeatureLevels = ARRAYSIZE(featureLevels);
+
+	DXGI_SWAP_CHAIN_DESC sd;
+	ZeroMemory(&sd, sizeof(sd));
+	sd.BufferCount = 1;
+	sd.BufferDesc.Width = width;
+	sd.BufferDesc.Height = heigth;
+	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	sd.BufferDesc.RefreshRate.Numerator = 60;
+	sd.BufferDesc.RefreshRate.Denominator = 1;
+	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	sd.OutputWindow = g_hWnd;
+	sd.SampleDesc.Count = 1;
+	sd.SampleDesc.Quality = 0;
+	sd.Windowed = true;
+
+	for (UINT driverTypeIndex = 0; driverTypeIndex < numDriverTypes; driverTypeIndex++)
+	{
+		g_driverType = driverTypes[driverTypeIndex];
+		hr = D3D11CreateDeviceAndSwapChain(NULL, g_driverType, NULL,
+			createDeviceFlag, featureLevels, numFeatureLevels,
+			D3D11_SDK_VERSION, &sd, &g_pSwapChain,
+			&g_pD3DDevice, &g_featrureLevel, &g_pImmediateContext);
+		if (SUCCEEDED(hr))
+			break;
+	}
+
+	if (FAILED(hr))
+		return hr;
+
+	//Tuto 2 Exer 1
+	// Get pointer to back buffer texture
+	ID3D11Texture2D* pBackBufferTexture;
+	hr = g_pSwapChain->GetBuffer(0, _uuidof(ID3D11Texture2D),
+		(LPVOID*)&pBackBufferTexture);
+
+	if (FAILED(hr)) return hr;
+
+	// Use the Pack huff,— texture pointer to create the render target view
+	hr = g_pD3DDevice->CreateRenderTargetView(pBackBufferTexture, NULL, &g_pBackBufferRTView);
+
+	pBackBufferTexture->Release();
+
+	if (FAILED(hr)) return hr;
+
+	// Set the render target view
+	g_pImmediateContext->OMSetRenderTargets(1, &g_pBackBufferRTView, NULL);
+
+	// Set the viewport
+	D3D11_VIEWPORT viewport
+		;
+	viewport.TopLeftX = 0;
+	viewport.TopLeftY = 0;
+	viewport.Width = (FLOAT)width;
+	viewport.Height = (FLOAT)heigth;
+	viewport.MinDepth = 0.0f;
+	viewport.MaxDepth = 1.0f;
+
+	g_pImmediateContext->RSSetViewports(1, &viewport);
+
+	return S_OK;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//Clean up D3D object
+//////////////////////////////////////////////////////////////////////////////
+void ShutdownD3D()
+{
+	if (g_pVertexBuffer) g_pVertexBuffer->Release();
+	if (g_pInputLayout) g_pInputLayout->Release();
+	if (g_pVertexShader) g_pVertexShader->Release();
+	if (g_pPixelShader) g_pPixelShader->Release();
+	if (g_pBackBufferRTView) g_pBackBufferRTView->Release();
+	if (g_pSwapChain) g_pSwapChain->Release();
+	if (g_pImmediateContext) g_pImmediateContext->Release();
+	if (g_pD3DDevice) g_pD3DDevice->Release();
+}
+
+////////////////////////////////////////////////////
+//Intit graphscs - Tutorial 03
+////////////////////////////////////////////////////
+HRESULT InitialiseGraphics()
+{
+	HRESULT hr = S_OK;
+
+	//Detine vertices of a triangle - screen coordinates -1.0 to +1.0
+	POS_COL_VERTEX vertices[] =
+	{
+		{XMFLOAT3(.92, 0.9f, 0.0f), XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f)},
+		{XMFLOAT3(0.9f, -0.9f, 0.0f), XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f)},
+		{XMFLOAT3(-0.9f, -0.5f, 0.0f), XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f)}
+	};
+	//Set up and create vertex buffer
+	D3D11_BUFFER_DESC bufferDesc;
+	ZeroMemory(&bufferDesc, sizeof(bufferDesc));
+	bufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+	bufferDesc.ByteWidth = sizeof(POS_COL_VERTEX) * 3;
+	bufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	hr = g_pD3DDevice->CreateBuffer(&bufferDesc, NULL, &g_pVertexBuffer);
+	//Create the buffer
+
+	if (FAILED(hr))//Resturn an error code if failed
+	{
+		return hr;
+	}
+
+	//Copy the vertices into the buffer
+	D3D11_MAPPED_SUBRESOURCE ms;
+
+	//Lock the buffer to allow writing
+	g_pImmediateContext->Map(g_pVertexBuffer, NULL, D3D11_MAP_WRITE_DISCARD, NULL, &ms);
+
+	//Copy the data
+	memcpy(ms.pData, vertices, sizeof(vertices));
+
+	//Unlock the buffer
+	g_pImmediateContext->Unmap(g_pVertexBuffer, NULL);
+
+	//Load and compile the pixel and vertex shaders - use vs_5_0 to target DX11 hardware only
+	ID3DBlob* VS, * PS, * error;
+	hr = D3DX11CompileFromFile("shaders.hlsl", 0, 0, "VShader", "vs_4_0", 0, 0, 0, &VS, &error, 0);
+
+	if (error != 0)//check for shader compilation error
+	{
+		OutputDebugStringA((char*)error->GetBufferPointer());
+		error->Release();
+		if (FAILED(hr))//Don't fail if error is just a warning
+		{
+			return hr;
+		}
+	}
+
+	hr = D3DX11CompileFromFile("shaders.hlsl", 0, 0, "PShader", "ps_4_0", 0, 0, 0, &PS, &error, 0);
+
+	if (error != 0)//check for shader compilation error
+	{
+		OutputDebugStringA((char*)error->GetBufferPointer());
+		error->Release();
+		if (FAILED(hr))//Don't fail if error is just a warning
+		{
+			return hr;
+		}
+	}
+
+	//Create shader objects
+	hr = g_pD3DDevice->CreateVertexShader(VS->GetBufferPointer(), VS->GetBufferSize(), NULL, &g_pVertexShader);
+
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+
+	hr = g_pD3DDevice->CreatePixelShader(PS->GetBufferPointer(), PS->GetBufferSize(), NULL, &g_pPixelShader);
+
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+
+	//Set shader objects as active
+	g_pImmediateContext->VSSetShader(g_pVertexShader, 0, 0);
+	g_pImmediateContext->PSSetShader(g_pPixelShader, 0, 0);
+
+	//Create and set the input layout object
+	D3D11_INPUT_ELEMENT_DESC iedesc[] =
+	{
+		//Be very carful setting the correct dxgi format and D3D version
+		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{"COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0},
+	};
+
+	hr = g_pD3DDevice->CreateInputLayout(iedesc, 2, VS->GetBufferPointer(), VS->GetBufferSize(), &g_pInputLayout);
+
+	if (FAILED(hr))
+	{
+		return hr;
+	}
+
+	g_pImmediateContext->IASetInputLayout(g_pInputLayout);
+
+	return S_OK;
+}
+
+// Render frame
+void RenderFrame(void)
+{
+	// Clear the back buffer - choose a colour you like
+	float rgba_clear_colour[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	g_pImmediateContext->ClearRenderTargetView(g_pBackBufferRTView, rgba_clear_colour);
+
+	//Tuto 3 Exer 1
+	//Set vertex buffer //03-01
+	UINT stride = sizeof(POS_COL_VERTEX);
+	UINT offset = 0;
+	g_pImmediateContext->IASetVertexBuffers(0, 1, &g_pVertexBuffer, &stride, &offset);
+
+	//Select whitch primtive type to use //03-02
+	g_pImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	//Draw the vertex buffer to back buffer //03-01
+	g_pImmediateContext->Draw(3, 0);
+
+	// RENDER HERE
+
+	// Display what has just been rendered
+	g_pSwapChain->Present(0, 0);
 }
